940b35b1a2de81f436e9b44eb7dfa56cb0574c45
SamueleOrazioDurante <samueleorazio.durante@drnt.dev>
2026-08-31 18:30:52 +0200

    fix: resolve absolute pathing issues for cargokit manifest directories and symlink evaluation on Windows

--- auth-sdk/flutter/flutter_opaque/rust_builder/cargokit/cmake/cargokit.cmake
+++ auth-sdk/flutter/flutter_opaque/rust_builder/cargokit/cmake/cargokit.cmake
@@ -33,10 +33,16 @@ function(apply_cargokit target manifest_dir lib_name any_symbol_name)
         set(CARGOKIT_TARGET_PLATFORM "windows-x64")
     endif()
 
+    if(IS_ABSOLUTE "${manifest_dir}")
+        set(_cargokit_manifest_dir "${manifest_dir}")
+    else()
+        set(_cargokit_manifest_dir "${CMAKE_CURRENT_SOURCE_DIR}/${manifest_dir}")
+    endif()
+
     set(CARGOKIT_ENV
         "CARGOKIT_CMAKE=${CMAKE_COMMAND}"
         "CARGOKIT_CONFIGURATION=$<CONFIG>"
-        "CARGOKIT_MANIFEST_DIR=${CMAKE_CURRENT_SOURCE_DIR}/${manifest_dir}"
+        "CARGOKIT_MANIFEST_DIR=${_cargokit_manifest_dir}"
         "CARGOKIT_TARGET_TEMP_DIR=${CARGOKIT_TEMP_DIR}"
         "CARGOKIT_OUTPUT_DIR=${CARGOKIT_OUTPUT_DIR}"
         "CARGOKIT_TARGET_PLATFORM=${CARGOKIT_TARGET_PLATFORM}"
